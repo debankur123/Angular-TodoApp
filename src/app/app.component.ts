8852a1a2260a8270bafa29f6be9d9cdf50abca18
Debankur Bhattacharjee <59233731+debankur123@users.noreply.github.com>
2023-02-07 22:08:23 +0530

    Undoing a task

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,16 +18,16 @@ export class AppComponent {
   add(newTask : string){
     this.tasks.push(new Task(newTask));
   }
-  
+
   remove(existingTask : Task){
-    var userConfirmed = confirm(`Are you sure you want to delete? \n "${existingTask}"`);
+    var userConfirmed = confirm(`Are you sure you want to delete? \n "${existingTask.title}"`);
     if(userConfirmed){
       this.tasks = this.tasks.filter(task => task != existingTask); 
     }
   }
 
-  markAsDone(task: Task){
-    task.isDone = true;
+  toggleIsDone(task: Task){
+    task.isDone = !task.isDone;
   }
 
 }
@@ -37,5 +37,10 @@ class Task{
   constructor(public title: string){
 
   }
+  toggleIsDone(){
+    this.isDone = !this.isDone;
+  }
+
+
   public isDone = false;
 }
