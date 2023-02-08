f70119e2c7fa907027256cc8a3e86c80a9c44926
Debankur-embee <debankur.b@embee.co.in>
2023-02-08 15:55:40 +0530

    Creating new Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+//import { Component } from '@angular/core';
 import { Component } from '@angular/core';
 
 @Component({
@@ -8,39 +9,4 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Todo-List'; 
   // We want to create some fields that are dynamic i.e which are not hardcoded.
-  tasks : Task[] =  [
-    new Task('Visit medical store'),
-    new Task('Go to Shop'),
-    new Task('Visit Office'),
-    new Task('Go to Gym'),
-    new Task('Going for Tutions')
-  ]
-  add(newTask : string){
-    this.tasks.push(new Task(newTask));
-  }
-
-  remove(existingTask : Task){
-    var userConfirmed = confirm(`Are you sure you want to delete? \n "${existingTask.title}"`);
-    if(userConfirmed){
-      this.tasks = this.tasks.filter(task => task != existingTask); 
-    }
-  }
-
-  toggleIsDone(task: Task){
-    task.isDone = !task.isDone;
-  }
-
-}
-
-class Task{
-
-  constructor(public title: string){
-
-  }
-  toggleIsDone(){
-    this.isDone = !this.isDone;
-  }
-
-
-  public isDone = false;
 }
