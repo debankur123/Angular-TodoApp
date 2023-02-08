f9ffbf557b39e7b4cf2916c418101a3fa617dea9
Debankur-embee <debankur.b@embee.co.in>
2023-02-08 18:15:31 +0530

    Update task-list Component

--- src/app/task-list/task-list.component.ts
+++ src/app/task-list/task-list.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-task-list',
+  selector: 'task-list',
   templateUrl: './task-list.component.html',
   styleUrls: ['./task-list.component.css']
 })
